0e4e89a3b3bab41e336ba6323d5a1b99729c8eb0
paulas91 <paulinaskopp@gmail.com>
2023-04-04 18:32:04 +0200

    First version

--- count_elements.rb
+++ count_elements.rb
@@ -1,3 +1,19 @@
 class CountElements
 
+  attr_reader :results
+
+  def initialize(results)
+    @results = results
+  end
+
+  def perform
+    @results.group_by(&:itself).transform_values(&:count)
+  end
 end
+
+
+
+
+
+
+
--- validate_password.rb
+++ validate_password.rb
@@ -1,3 +1,17 @@
 class ValidatePassword
 
+  attr_reader :password
+
+  def initialize(password)
+    @password = password
+  end
+
+  def perform
+    if (6..24).include?(@password.size) && @password.scan(/[a-z]/) && @password.scan(/\d/) && @password.scan(/A-Z/) && @password.scan(/\d/) && @password.scan(/[a-z]{1,2}/)
+      true
+    else
+      false
+    end
+
+  end
 end
