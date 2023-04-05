31b87c5a213bec545331aba669329744cae3ff6c
paulas91 <paulinaskopp@gmail.com>
2023-04-05 09:52:01 +0200

    task ValidatePassword

--- validate_password.rb
+++ validate_password.rb
@@ -1,17 +1,30 @@
-class ValidatePassword
 
-  attr_reader :password
+class ValidatePassword
+  VALIDATION_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%&*+=:;?<>])[A-Za-z\d!@#$%&*+=:;?<>]{6,24}$/
+  private_constant :VALIDATION_REGEX
 
   def initialize(password)
     @password = password
   end
 
   def perform
-    if (6..24).include?(@password.size) && @password.scan(/[a-z]/) && @password.scan(/\d/) && @password.scan(/A-Z/) && @password.scan(/\d/) && @password.scan(/[a-z]{1,2}/)
-      true
-    else
-      false
-    end
+    valid_regex? && valid_repetition?
+  end
 
+  private
+
+  attr_reader :password
+
+  def valid_regex?
+    !!(password =~ VALIDATION_REGEX)
+  end
+
+  def valid_repetition?
+    chars = password.chars
+    !chars.find.with_index do |char, i|
+      break if i == chars.count - 2
+
+      [char, chars[i + 1], chars[i + 2]].uniq.count == 1
+    end
   end
 end
